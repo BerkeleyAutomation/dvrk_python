1ade4e367f8f22568938dce1f5931b64ab9000a1
Aditya Ganapathi <4732720+adijunn@users.noreply.github.com>
2019-08-26 11:37:33 -0700

    cloth height consiered

--- tests/test_03_checkerboard_pick_test.py
+++ tests/test_03_checkerboard_pick_test.py
@@ -1,6 +1,6 @@
 """Test if calibration is working. This can be run as a stand alone file, i.e.:
 
-python test_02_checkerboard_test.py
+python test_03_checkerboard_pick_test.py
 
 should be enough. Be careful that the position is safe!
 """
@@ -11,11 +11,12 @@ from dvrkClothSim import dvrkClothSim
 from os import path
 np.set_printoptions(suppress=True)
 import utils as U
-
+import time
 
 if __name__ == "__main__":
     row_board = 6
     column_board = 6
+    cloth_height = 0.005    # unit = (m)
 
     if path.exists('mapping_table'):
         # import data from file
@@ -29,6 +30,7 @@ if __name__ == "__main__":
         for j in range(column_board):
             data_default[cnt,0] = -1+j*0.4
             data_default[cnt,1] = -1+i*0.4
+            data_default[cnt,4] = data_default[cnt,4] + cloth_height
             cnt += 1
     data = data_default
 
@@ -44,7 +46,6 @@ if __name__ == "__main__":
 
     p = dvrkClothSim()
     p.set_position_origin([0.003, 0.001, -0.06], 0, 'deg')
-
     for i in range(row_board):
         for j in range(column_board):
             x = -1+j*0.4
@@ -53,3 +54,4 @@ if __name__ == "__main__":
             dy = 0.0
             U.move_p_from_net_output(x, y, dx, dy, row_board, column_board, data_square, p)
             cnt += 1
+            time.sleep(1)
